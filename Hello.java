2a68b07756b3c737df07543ffacf447a21fd36e5
Admin <darrel131191@gmail.com>
2020-05-28 17:04:26 +0800

    This is my Second Commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World123456");
+      System.out.println("Hello, World1236");
    }
 }
